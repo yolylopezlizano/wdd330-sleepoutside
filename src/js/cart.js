364faa00ccdc8d8623bc413341bf264ecf59410f
yolylizano <yolylizano@gmail.com>
2025-10-31 10:42:05 -0500

    change cart 3

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,22 +7,33 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
+  // Adjust the image path depending on the file location
+  let imagePath = item.Image;
+
+  // If the image path is not an absolute URL and not already correct
+  if (!imagePath.startsWith("http")) {
+    // Fix relative paths to work correctly from /cart/ folder
+    imagePath = imagePath.replace(/^(\.\.\/)+/, "../");
+  }
+
+  // HTML template for each item in the cart
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    <a href="#" class="cart-card__image">
+      <img
+        src="${imagePath}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
 
   return newItem;
 }
 
+// Render the cart when the page loads
 renderCartContents();
