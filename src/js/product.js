f7888b42ad74d212e175ca6618a022cf2a3adfed
yolylizano <yolylizano@gmail.com>
2025-10-31 10:28:54 -0500

    changes in cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,11 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  // add the new product
+  cart.push(product);
+  // save the changes into the cart
+  localStorage.setItem("so-cart", JSON.stringify(cart));
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
