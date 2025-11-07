5652f71aef8decfbbc973c54ca3bb54437bf39c5
yolylizano <yolylizano@gmail.com>
2025-11-07 12:09:22 -0600

    Build fixed: products load correctly in Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -11,20 +11,23 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
-        ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
+        product: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "json/*", // 👈 copia todo lo de src/json
+          dest: "json",  // 👈 crea dist/json/
+        },
+        {
+          src: "images/**/*", // 👈 copia todas las imágenes
+          dest: "images",     // 👈 crea dist/images/
+        },
+      ],
+    }),
+  ],
 });
+
